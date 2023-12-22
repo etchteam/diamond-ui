de5fd07e2fd2a8b6b5002200f8b2370f38aa470f
Dan Webb <dan@etch.co>
2023-12-22 09:57:30 +0000

    feat: typescript and web dev server

---
+++ web-dev-server.config.js
@@ -0,0 +1,11 @@
+const config = {
+  open: true,
+  watch: true,
+  rootDir: 'documentation',
+  appIndex: 'index.html',
+  nodeResolve: {
+    exportConditions: ['development'],
+  },
+};
+
+export default config;
