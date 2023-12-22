8bcf805a72c869ecdab3fd49b40b1b09032db573
Dan Webb <dan@etch.co>
2023-12-22 18:56:31 +0000

    feat: separate documentation

---
+++ documentation/index.js
@@ -0,0 +1,4 @@
+import '@etchteam/diamond-ui/dist/composition/container/container.js';
+import '@etchteam/diamond-ui/dist/canvas/card/card.js';
+import '@etchteam/diamond-ui/dist/content/img/img.js';
+import '@etchteam/diamond-ui/dist/control/button/button.js';
