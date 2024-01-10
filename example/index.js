a77ff73d4f447fcc0c3ddc3c98b998f72fa012e0
Gavyn McKenzie <gavyn@etch.co>
2024-01-10 17:26:44 +0000

    feat: add exports to package to clean up imports and link to local package

--- example/index.js
+++ example/index.js
@@ -1,4 +1,4 @@
-import '@etchteam/diamond-ui/dist/composition/container';
-import '@etchteam/diamond-ui/dist/canvas/card';
-import '@etchteam/diamond-ui/dist/content/img';
-import '@etchteam/diamond-ui/dist/control/button';
+import '@etchteam/diamond-ui/composition/container/container';
+import '@etchteam/diamond-ui/canvas/card/card';
+import '@etchteam/diamond-ui/content/img/img';
+import '@etchteam/diamond-ui/control/button/button';
