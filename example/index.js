9acc87360546a72bb5a737d0388e55bede0df2b3
Gavyn McKenzie <gavyn@etch.co>
2024-01-10 17:14:23 +0000

    refactor: move documentation to example

---
+++ example/index.js
@@ -0,0 +1,4 @@
+import '@etchteam/diamond-ui/dist/composition/container';
+import '@etchteam/diamond-ui/dist/canvas/card';
+import '@etchteam/diamond-ui/dist/content/img';
+import '@etchteam/diamond-ui/dist/control/button';
