20abea45e874e414e6fe955e101a26b32bb3b362
Dan Webb <dan@etch.co>
2024-01-23 08:10:51 +0000

    fix: add extra semantic release config

--- release.config.cjs
+++ release.config.cjs
@@ -1,13 +1,30 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {},
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    '@semantic-release/changelog',
     [
       '@semantic-release/npm',
       {
+        npmPublish: true,
         pkgRoot: 'dist',
       },
     ],
+    '@semantic-release/github',
+
+    // NOTE: this plugin must be last to ensure all updated files are committed
+    '@semantic-release/git',
   ],
 };
