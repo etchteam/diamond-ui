7c5ecca735edf9ed294061b56432586a133af8da
Gavyn McKenzie <gavyn@etch.co>
2024-02-05 11:24:39 +0000

    feat: spacing utility class

---
+++ components/composition/Spacing/Spacing.stories.ts
@@ -0,0 +1,49 @@
+import { StoryObj } from '@storybook/web-components';
+import { html } from 'lit';
+
+import '../../canvas/Card/Card';
+
+const description = `
+Diamond spacing is a set of utility classes that can be added to any component or element.
+
+Spacing tweaks are one of the most common reasons for introducing page level CSS, which is
+a slippery slope to a bloated CSS file. Diamond spacing reduces or eliminates these tweaks.
+`;
+
+export default {
+  component: 'diamond-spacing',
+  parameters: {
+    docs: {
+      description: {
+        component: description,
+      },
+    },
+  },
+  argTypes: {
+    size: {
+      control: { type: 'inline-radio' },
+    },
+    options: ['xs', 'sm', 'md', 'lg', 'xl'],
+  },
+};
+
+export const Spacing: StoryObj = {
+  render: (args) => html`
+    <div
+      class="diamond-spacing-bottom-${args.size}"
+      style="height: 100px; background-color: #eee;"
+    ></div>
+    <div
+      class="diamond-spacing-bottom-${args.size}"
+      style="height: 100px; background-color: #eee;"
+    ></div>
+    <div
+      class="diamond-spacing-bottom-${args.size}"
+      style="height: 100px; background-color: #eee;"
+    ></div>
+  `,
+};
+
+Spacing.args = {
+  size: 'sm',
+};
