d71cc8df48d977bd810ad551ddff307efa3a2675
Carl Mensah <carl@etch.co>
2024-03-04 14:37:08 +0000

    feat: update story example and css

--- components/composition/LoadingSkeleton/LoadingSkeleton.stories.ts
+++ components/composition/LoadingSkeleton/LoadingSkeleton.stories.ts
@@ -25,16 +25,30 @@ export default {
 
 export const Loading: StoryObj = {
   render: (args) => html`
+  <div style="height: 600px; display:flex; justify-content:center;">
     <diamond-wrap size="md" gutter="md">
       <diamond-loading-skeleton loading="${args.loading}">
+        <h2>Example page title</h3>
         <diamond-card border="" padding="md" class="diamond-theme-light">
-          <h3>Card title</h3>
+        <h3>Card title</h3>
+          <diamond-img>
+            <img
+              src="https://placehold.it/300x300"
+              alt="Placeholder"
+              width="300"
+              height="300"
+            />
+          </diamond-img>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
+          <diamond-button>
+            <a href="#" onclick="return false;">Anchor</a>
+          </diamond-button>
         </diamond-card>
       </diamond-loading-skeleton>
     </diamond-wrap>
+  </div>
   `,
 };
