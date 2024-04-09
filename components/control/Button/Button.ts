92e762634a665113bdd6da509bd4fcb784b91a87
Carl Mensah <carl@etch.co>
2024-04-09 17:48:00 +0100

    fix: return button attributes

--- components/control/Button/Button.ts
+++ components/control/Button/Button.ts
@@ -2,6 +2,7 @@ import { JSXCustomElement } from '../../../types/jsx-custom-element';
 
 export interface ButtonAttributes {
   size?: 'sm' | 'md' | 'lg';
+  variant?: 'primary' | 'secondary' | 'text';
   width?: 'full-width' | 'full-width-mobile' | 'square';
 }
 
