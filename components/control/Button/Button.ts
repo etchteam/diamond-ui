3ceef8447c001dedbdd00b8d95c2d1dbcc27a1d4
Carl Mensah <carl@etch.co>
2024-04-12 15:38:14 +0100

    chore: revert button styles

--- components/control/Button/Button.ts
+++ components/control/Button/Button.ts
@@ -6,16 +6,6 @@ export interface ButtonAttributes {
   width?: 'full-width' | 'full-width-mobile' | 'square';
 }
 
-export class DiamondButton extends HTMLElement {
-  constructor() {
-    super();
-    // Ensure the enforced class is always applied to the custom element
-    this.classList.add('diamond-button-shared');
-  }
-}
-
-customElements.define('diamond-button', DiamondButton);
-
 declare global {
   interface HTMLElementTagNameMap {
     'diamond-button': ButtonAttributes;
