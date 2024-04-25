b53e0608686b976ffaa8c846e2a4f191ec589d15
Gavyn McKenzie <gavyn@etch.co>
2024-04-25 11:58:32 +0100

    docs: upgrade to storybook 8

---
+++ docs/recipes/Theming.stories.ts
@@ -0,0 +1,44 @@
+import { StoryObj } from '@storybook/web-components';
+import { html } from 'lit';
+
+import '../../components/composition/Grid/Grid';
+import '../../components/composition/Grid/GridItem';
+import '../../components/canvas/Card/Card';
+
+export default {
+  title: 'Recipes/Theming',
+};
+
+export const Theming: StoryObj = {
+  render: () => html`
+    <diamond-grid wrap="wrap">
+      <diamond-grid-item small-mobile="12" tablet="4">
+        <diamond-card border class="diamond-theme-light">
+          <h3>Card title</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </diamond-card>
+      </diamond-grid-item>
+      <diamond-grid-item small-mobile="12" tablet="4">
+        <diamond-card border class="diamond-theme-medium">
+          <h3>Card title</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </diamond-card>
+      </diamond-grid-item>
+      <diamond-grid-item small-mobile="12" tablet="4">
+        <diamond-card border class="diamond-theme-dark">
+          <h3>Card title</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </diamond-card>
+      </diamond-grid-item>
+    </diamond-grid>
+  `,
+};
